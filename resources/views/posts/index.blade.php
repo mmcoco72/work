7ff3946cbb411e1b3a4b49fb1ff98219ac305415
mmcoco72 <bad.ema.1011@gmail.com>
2022-06-22 22:31:21 +0900

    add_posts_paginate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,9 +7,12 @@
                 @foreach ($posts as $post)
                     <div class='post'>
                         <h3 class='title'>{{ $post->title }}</h3>
-                        <h3 class='body'>{{ $post->body }}</h3>
+                        <p class='body'>{{ $post->body }}</p>
                     </div>
                 @endforeach
+           </div>
+           <div class='paginate'>
+               {{ $posts->links() }}
            </div>
         </body>
 @endsection
