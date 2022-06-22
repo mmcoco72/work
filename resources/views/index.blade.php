2f7eb66af258741691c7eb9a17f43ac3f56aff98
mmcoco72 <bad.ema.1011@gmail.com>
2022-06-22 12:02:31 +0900

    create_index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')　　　　　　　　　　　　　　　　　　
+
+@section('content')
+        <div class="body">
+            <div class="menu">
+                <h4 class='topictop'>日記一覧</h4>
+                <p>作成</p>
+                <h4 class='topictop'>タスク一覧</h4>
+                <p>作成</p>
+                <h4 class='topictop'>コミュニティ</h4>
+            </div>
+            <div class="calendar">
+                <div id='calendar'></div>
+            </div>
+        </div>
+@endsection
